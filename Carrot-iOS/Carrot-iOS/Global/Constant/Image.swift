116e6d5774453a9054c4d7e7a44eb8ea7d13e8d0
timotheekim10 <timotheekim10@gmail.com>
2023-11-20 02:08:24 +0900

    [Add] 이미지 Assets 추가 #6

---
+++ Carrot-iOS/Carrot-iOS/Global/Constant/Image.swift
@@ -0,0 +1,54 @@
+//
+//  Image.swift
+//  Carrot-iOS
+//
+//  Created by 티모시 킴 on 11/20/23.
+//
+
+import UIKit
+
+public enum Image {
+    
+    //MARK: - Icon
+    
+    static let add = UIImage(named: "ic_add")!
+    static let alarm = UIImage(named: "ic_alarm")!
+    static let alert = UIImage(named: "ic_alert")!
+    static let camera = UIImage(named: "ic_camera")!
+    static let chatting = UIImage(named: "ic_chatting")!
+    static let comment = UIImage(named: "ic_comment")!
+    static let create = UIImage(named: "ic_create")!
+    static let gallery = UIImage(named: "ic_gallery")
+    static let hamburger = UIImage(named: "ic_hamburger")
+    static let home = UIImage(named: "ic_home")
+    static let life = UIImage(named: "ic_life")
+    static let likedCircle = UIImage(named: "ic_liked_circle")
+    static let liked = UIImage(named: "ic_liked")
+    static let location = UIImage(named: "ic_location")
+    static let meatballCircle = UIImage(named: "ic_meatball_circle")
+    static let meatball = UIImage(named: "ic_meatball")
+    static let mypage = UIImage(named: "ic_mypage")
+    static let navigationLeft = UIImage(named: "ic_navigation_left")
+    static let navigationRight = UIImage(named: "ic_navigation_right")
+    static let navigationUnder = UIImage(named: "ic_navigation_under")
+    static let nearby = UIImage(named: "ic_nearby")
+    static let personnel = UIImage(named: "ic_personnel")
+    static let popular = UIImage(named: "ic_popular")
+    static let profile = UIImage(named: "ic_profile")
+    static let search = UIImage(named: "ic_search")
+    static let searchclub = UIImage(named: "ic_searchclub")
+    static let shareCircle = UIImage(named: "ic_share_circle")
+    static let time = UIImage(named: "ic_time")
+    
+    //MARK: - Dummy
+    
+    static let dummyMain = UIImage(named: "img_2")
+    static let dummyClub1 = UIImage(named: "img_8")
+    static let dummyClub2 = UIImage(named: "img_18")
+    static let dummyClub3 = UIImage(named: "img_14")
+    static let dummyDetail1 = UIImage(named: "img_27")
+    static let dummyDetail2 = UIImage(named: "img_28")
+    static let dummyDetail3 = UIImage(named: "img_29")
+    static let dummySignUp = UIImage(named: "Group 45")
+    
+}
