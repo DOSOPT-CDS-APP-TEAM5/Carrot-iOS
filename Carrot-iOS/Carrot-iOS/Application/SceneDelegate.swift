41c616025d56e60c04f5cb6e4682af0955bc5aa0
YunHak Lee <lyh990408@gmail.com>
2023-11-27 13:15:57 +0900

    [Fix] 앱 rootView 다시 mainVC로 되돌림-2 #31

--- Carrot-iOS/Carrot-iOS/Application/SceneDelegate.swift
+++ Carrot-iOS/Carrot-iOS/Application/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
         window?.overrideUserInterfaceStyle = .light 
-
+        
         let mainViewController = MainViewController()
         let navigationController = UINavigationController(rootViewController: mainViewController)
 
